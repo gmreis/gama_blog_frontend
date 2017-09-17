e6392c7151a65fd68504f919eec5d4e5a80c944e
Tiago Coelho <tiagocoelho33@gmail.com>
2017-09-17 20:18:47 -0300

    google analytics

--- app/app.config.js
+++ app/app.config.js
@@ -15,4 +15,18 @@ angular.
         }).
         otherwise('/home');
     }
-  ]);
+  ]).
+  run(function($rootScope, $window) { 
+    $rootScope.$on("$routeChangeStart",function(event, next, current){
+        // Caso o evento do angular seja de alteracao de rota disparar o evento customizado
+        if(next.templateUrl) {
+            // Evento customizado do google tag manager
+            $window.dataLayer.push(
+              {
+                'event': 'routeChange',
+                'virtualPageView': next.templateUrl
+              }
+            );
+        }
+    });
+  });
