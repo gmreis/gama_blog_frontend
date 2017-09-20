090ebd6f0cb05866ca13aeb7f5e9c9c56e44cd91
Tiago Coelho <tiagocoelho33@gmail.com>
2017-09-20 00:10:53 -0300

    facebook pixel

--- app/app.config.js
+++ app/app.config.js
@@ -26,12 +26,15 @@ angular.
     }
   ]).
   run(function($rootScope, $window, $location) { 
+    $window.fbq('init', '111425179604541');
     $window.ga('create', 'UA-106574119-1', 'auto');
     $rootScope.$on('$locationChangeSuccess', function(event) {
       // console.log('locationChangeSuccess');
       if (!$window.ga){
         return;
       }
+      // console.log("teste "+$location.path());
+      $window.fbq('track', 'PageView', { page: $location.path() });
       $window.ga('send', 'pageview', { page: $location.path() });
     });
   });
